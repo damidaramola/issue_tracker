45ff0b37dd2cf2f2ac2620d6468db0d90baf528d
damidaramola <damidaramola7gmail@example.com>
2025-08-02 12:10:01 +0100

    add components and fetch user data

---
+++ app/Components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+            <button onClick={() => console.log('Can you see me?')}>Add to cart</button>
+
+    </div>
+  )
+}
+
+export default AddToCart
---
+++ app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPage = () => {
+  return (
+    <div>
+      new User page!
+    </div>
+  )
+}
+
+export default NewPage
