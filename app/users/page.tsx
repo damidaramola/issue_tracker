ce2eb28f4bf7bcefd449181b00ee1b808d283fac
damidaramola <damidaramola7gmail@example.com>
2025-08-03 01:00:50 +0100

    style pages w daisy UI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 interface User {
     id:number;
     name: string;
+    email: string;
 }
 
 const UsersPage = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const res = await fetch("https://jsonplaceholder.typicode.com/users",{cache: 'no-store'}); /* caching - revalidate data every 10 seconds*/
     const users:User[] = await res.json();
   return (
    <>
    <h1>Users</h1>
-   <ul>
-     {users.map(user=><li key={user.id}>{user.name}</li>)}
-   </ul>
+   <table className='table table-bordered'>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+     {users.map(user=><tr key={user.id}>
+        <th>{user.name}</th>
+        <th>{user.email}</th>
+     </tr>)}
+     </tbody>
+   </table>
    </>
    
   )
